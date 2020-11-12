7a67707b591d64b8c6f2d24b155fa71aa4ce9a3d
John Gibson <jg20019@gmail.com>
2020-11-12 15:41:17 -0500

    Changes player after making move.

--- game.js
+++ game.js
@@ -1,7 +1,19 @@
 
-Gameboard = ((el) => {
+function Player(name, symbol) {
+    const wins = 0;  
+
+    const getName = () => name ; 
+    const getSymbol = () => symbol; 
+    const getWins = () => wins; 
+    const increaseWins = () => {
+        wins++; 
+    }; 
+
+    return { getName, getSymbol, getWins, increaseWins }; 
+};  
+
+Board = (el) => {
     let board = Array(9).fill(''); 
-    let player = 'X'; 
 
     el.innerHTML = `
         <div class="board">
@@ -19,8 +31,10 @@ Gameboard = ((el) => {
 
     const handleClick = (i) => {
         return () => {
-            board[i] =  'X'; // TODO: get current player  
-            renderBoard(); 
+            el.dispatchEvent(new CustomEvent('Move', {
+                bubbles: true, 
+                detail: { position: i }, 
+            })); 
         }; 
     }; 
 
@@ -28,7 +42,12 @@ Gameboard = ((el) => {
     for(let i = 0; i < squares.length; i++) {
         squares[i].addEventListener('click', handleClick(i)); 
     } 
-    
+   
+    function placeMove(symbol, position) {
+        board[position] = symbol; 
+        renderBoard(); 
+    } 
+
     function renderBoard() {
         let squares = el.querySelectorAll('.board div'); 
         for(let i = 0; i < squares.length; i++) {
@@ -40,5 +59,38 @@ Gameboard = ((el) => {
         return console.log(board); 
     };  
 
-    return { renderBoard, showBoard } 
+    return { placeMove, renderBoard, showBoard } 
+}; 
+
+Game = ((el) => {
+    el.innerHTML = `
+        <div class="game">
+            <div class="player1"></div> 
+            <div class="player2"></div> 
+            <div class="board-root"></div> 
+        </div> 
+    `; 
+
+    const player1 = Player('John', 'X'); 
+    const player2 = Player('Sarah', 'O'); 
+    let currentPlayer = player1; 
+
+    const board = Board(el.querySelector('.board-root')); 
+
+    el.addEventListener('Move', function (e) {
+        board.placeMove(currentPlayer.getSymbol(), e.detail.position); 
+        swapTurns(); 
+    }); 
+
+    function swapTurns() {
+        if (player1.getSymbol() === currentPlayer.getSymbol()) {
+            console.log('Changing players.'); 
+            currentPlayer = player2; 
+        } else {
+            currentPlayer = player1; 
+            console.log('Not changing players.'); 
+        } 
+    }
+    
 })(document.querySelector('#root')); 
+

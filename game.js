fb63d77bf3ac996adb0d26e7a60a7968820bc169
John Gibson <jg20019@gmail.com>
2020-11-12 15:20:39 -0500

    Can make moves on board.

--- game.js
+++ game.js
@@ -1,11 +1,13 @@
 
 Gameboard = ((el) => {
     let board = Array(9).fill(''); 
+    let player = 'X'; 
+
     el.innerHTML = `
         <div class="board">
-            <div>X</div> 
-            <div>X</div> 
-            <div>X</div> 
+            <div></div> 
+            <div></div> 
+            <div></div> 
             <div></div> 
             <div></div> 
             <div></div>
@@ -15,14 +17,26 @@ Gameboard = ((el) => {
         </div> 
     `; 
 
-    const renderBoard = () => {
+    const handleClick = (i) => {
+        return () => {
+            board[i] =  'X'; // TODO: get current player  
+            renderBoard(); 
+        }; 
+    }; 
+
+    const squares = el.querySelectorAll('.board div'); 
+    for(let i = 0; i < squares.length; i++) {
+        squares[i].addEventListener('click', handleClick(i)); 
+    } 
+    
+    function renderBoard() {
         let squares = el.querySelectorAll('.board div'); 
-        for(let i = 0; i <= squares.length; i++) {
+        for(let i = 0; i < squares.length; i++) {
             squares[i].innerText = board[i]; 
         } 
     }; 
 
-    const showBoard = () => {
+    function showBoard() {
         return console.log(board); 
     };  
 
